2fc1489436aa6069fdbd8a3d1fde2a7a1cfaea39
ciao0603 <jocelyn94032.0@gmail.com>
2024-01-29 10:43:25 +0800

    fix typo in app.js

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ app.use(session({
 }))
 app.use(flash())
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')
-  res.locals.error_messages = req.flash('error_messages')
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
   next()
 })
 
