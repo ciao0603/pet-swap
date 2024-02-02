62bd5985df905054c776f44ce1905959f54c5a21
ciao0603 <jocelyn94032.0@gmail.com>
2024-02-02 16:03:24 +0800

    feat: Add Category & SubCategory model

---
+++ migrations/20240202074648-create-sub-category.js
@@ -0,0 +1,36 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Sub_categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Categories',
+          key: 'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Sub_categories')
+  }
+}
