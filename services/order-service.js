ed7719abf0d324a68a4e029dcc78f86da715fbc2
ciao0603 <jocelyn94032.0@gmail.com>
2024-06-18 16:44:41 +0800

    modify: Use Promise.all to process async operation

--- services/order-service.js
+++ services/order-service.js
@@ -16,17 +16,14 @@ const orderService = {
       const order = await Order.create({ userId, receiverName, receiverPhone, receiverAddress, totalPrice })
       // 紀錄訂單詳情
       const productIdArray = Array.isArray(productId) ? productId : [productId]
-      productIdArray.forEach(async pid => {
-        await OrderItem.create({
-          orderId: order.id,
-          productId: pid
-        })
+      await Promise.all(productIdArray.map(async pid => {
+        await OrderItem.create({ orderId: order.id, productId: pid })
         // 將商品紀錄為已售出
         const product = await Product.findByPk(pid)
         await product.update({ buyerUserId: userId })
         // 刪除購物車紀錄
         await Cart.destroy({ where: { productId: pid } })
-      })
+      }))
 
       cb(null)
     } catch (err) {
